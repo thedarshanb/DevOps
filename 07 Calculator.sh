340069d2aef559a805eb0bb28b23f31f0b616de3
Darshan <thedarshanb@gmail.com>
2025-12-05 22:27:34 +0530

    07

---
+++ 07 Calculator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter the Numbers:"
+
+read num1
+read num2
+
+
+echo "The sum of Addition :$((num1+num2))"
+echo "The sum of Subtraction :$((num1-num2))"
+echo "The sum of Multiplication :$((num1*num2))"
+echo "The sum of Division :$((num1/num2))"
